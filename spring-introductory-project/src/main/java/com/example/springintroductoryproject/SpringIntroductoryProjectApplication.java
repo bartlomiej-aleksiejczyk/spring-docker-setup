9178f5ec28609baa1140eb884e23f12d74053d39
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-22 03:37:05 +0200

    Added working spring and postgresql configuration

---
+++ spring-introductory-project/src/main/java/com/example/springintroductoryproject/SpringIntroductoryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintroductoryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class SpringIntroductoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductoryProjectApplication.class, args);
+	}
+
+}
---
+++ spring-introductory-project/src/test/java/com/example/springintroductoryproject/SpringIntroductoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintroductoryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
