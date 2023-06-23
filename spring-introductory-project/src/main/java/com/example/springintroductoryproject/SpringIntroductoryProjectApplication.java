b5c51be855868912bc6b3aaabc3e4f35e43f2c4c
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-23 02:16:02 +0200

    Improved Spring configuration, added test, works well with linux

--- spring-introductory-project/src/main/java/com/example/springintroductoryproject/SpringIntroductoryProjectApplication.java
+++ spring-introductory-project/src/main/java/com/example/springintroductoryproject/SpringIntroductoryProjectApplication.java
@@ -2,6 +2,13 @@ package com.example.springintroductoryproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication 
 public class SpringIntroductoryProjectApplication {
@@ -9,5 +16,18 @@ public class SpringIntroductoryProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntroductoryProjectApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
