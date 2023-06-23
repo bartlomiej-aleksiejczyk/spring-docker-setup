7fcb7e344be8c42bc925d0c31a112ab46cbbc9cd
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-23 03:59:35 +0200

    Added new configuration with unit test setub

--- spring-introductory-project/src/main/java/com/example/springintroductoryproject/HelloController.java
+++ spring-introductory-project/src/main/java/com/example/springintroductoryproject/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot! Martyna Kocham CB :*";
     }
-    private static int add(int int1, int int2)
+    public int add(int int1, int int2)
     {
         int sum = int1 + int2;
         return sum;
---
+++ spring-introductory-project/src/test/java/com/example/springintroductoryproject/HelloControllerTests.java
@@ -0,0 +1,21 @@
+package com.example.springintroductoryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class HelloControllerTests {
+    @Test
+    void onePlusOneShouldGiveTwo() {
+        var calculation = new HelloController();
+        assertEquals(2,calculation.add(1,1));
+
+    }
+    @Test
+    void onePlusOneShouldGiveThree() {
+        var calculation = new HelloController();
+        assertEquals(3,calculation.add(1,1));
+
+    }
+}
