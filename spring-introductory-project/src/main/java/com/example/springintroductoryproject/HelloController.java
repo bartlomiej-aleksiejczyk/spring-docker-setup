3b5602f23f15ce0d23c7f5ae5a3ea3dec329b8eb
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-23 03:26:49 +0200

    Added newest confuiguration with working debugger

--- spring-introductory-project/src/main/java/com/example/springintroductoryproject/HelloController.java
+++ spring-introductory-project/src/main/java/com/example/springintroductoryproject/HelloController.java
@@ -10,5 +10,9 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot! Martyna Kocham CB :*";
     }
-
+    private static int add(int int1, int int2)
+    {
+        int sum = int1 + int2;
+        return sum;
+    }
 }
